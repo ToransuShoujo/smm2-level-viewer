0123b4f421899308d8f5023fa1cee93946aa7d9f
Emily <toransushojo@hotmail.com>
2021-11-17 23:24:25 -0500

    Cleaned up mushroom platform code

--- app/assets/js/objects/terrain/mushroomplatform.js
+++ app/assets/js/objects/terrain/mushroomplatform.js
@@ -19,63 +19,61 @@ class MushroomPlatform extends Terrain {
     }
 
     // Hack to fix draw order. Please, future self, find a better way
-		this.data.position.y_real = this.data.position.y;
-		this.data.position.y = (this.data.position.y + this.data.dimensions.height);
+    this.data.position.y_real = this.data.position.y;
+    this.data.position.y = (this.data.position.y + this.data.dimensions.height);
 
   }
 
   draw() {
-    for (let x = 1; x < this.data.dimensions.width; x++) {
+    for (let x = 0; x < this.data.dimensions.width; x++) {
       for (let y = 0; y < this.data.dimensions.height; y++) {
         let offset;
 
-        if (x == 1) {
-          if (y !== this.data.dimensions.height - 1) {
-            break;
-          } else {
+        //If we are at the top of the mushroom platform...
+        if (y == this.data.dimensions.height - 1) {
+          //If we are at the very left of the top section...
+          if (x == 0) {
             offset = this.spriteOffset.top_left;
-          }
-        } else if (x == this.data.dimensions.width) {
-          if (y !== this.data.dimensions.height - 1) {
-            break;
+            //Else if we are at the very right of the top section...
+          } else if (x == this.data.dimensions.width - 1) {
+            offset = this.spriteOffset.top_right
           } else {
-            offset = this.spriteOffset.top_right;
+            offset = this.spriteOffset.top_middle;
           }
+          //If we are not at the top of the mushroom platform...
         } else {
-          if ((this.data.dimensions.width) % 2 !== 0) {
-            if (y !== this.data.dimensions.height - 1) {
-              if (x == (this.data.dimensions.width / 2) + 0.5) {
-                offset = this.spriteOffset.center;
-              } else {
-                break;
-              }
-            } else {
-              offset = this.spriteOffset.top_middle;
-            }
+          //If we are at the center of the mushroom platform...
+          if ((x == this.data.dimensions.width / 2) || (x == (this.data.dimensions.width / 2) - 0.5)) {
+            offset = this.spriteOffset.center;
           } else {
-            if (y !== this.data.dimensions.height - 1) {
-              if (x == (this.data.dimensions.width / 2) - 1) {
-                offset = this.spriteOffset.center_offset_left;
-              } else if (x == (this.data.dimensions.width / 2)) {
-                offset = this.spriteOffset.center_offset_right;
-              } else {
-                break;
-              }
-            } else {
-              offset = this.spriteOffset.top_middle;
-            }
+            break;
           }
         }
-        this.canvasContext.drawImage(
-  				this.scene.spriteSheet,
-  				this.spriteSheetThemeOffset.x + offset.x,
-  				this.spriteSheetThemeOffset.y + offset.y,
-  				offset.width,
-  				offset.height,
-  				this.data.position.x + x - 1,
-  				(this.scene.canvas.height - this.data.position.y_real) - y,
-  				1, 1
-  			);
+
+        //If the mushroom platform is of even width AND we are not at the top of the section...
+        if ((y !== this.data.dimensions.height - 1) && (this.data.dimensions.width % 2 == 0)) {
+          this.canvasContext.drawImage(
+            this.scene.spriteSheet,
+            this.spriteSheetThemeOffset.x + offset.x,
+            this.spriteSheetThemeOffset.y + offset.y,
+            offset.width,
+            offset.height,
+            this.data.position.x + x - 1,
+            (this.scene.canvas.height - this.data.position.y_real) - y,
+            1, 1
+          );
+        } else {
+          this.canvasContext.drawImage(
+            this.scene.spriteSheet,
+            this.spriteSheetThemeOffset.x + offset.x,
+            this.spriteSheetThemeOffset.y + offset.y,
+            offset.width,
+            offset.height,
+            this.data.position.x + x,
+            (this.scene.canvas.height - this.data.position.y_real) - y,
+            1, 1
+          );
+        }
       }
     }
   }
