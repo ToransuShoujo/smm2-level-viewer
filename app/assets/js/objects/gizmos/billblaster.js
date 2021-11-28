041fe103fabf29bc2d0879419b248717de0b89f5
Emily <toransushojo@hotmail.com>
2021-11-27 23:09:52 -0500

    Fixed bullet blasters

--- app/assets/js/objects/gizmos/billblaster.js
+++ app/assets/js/objects/gizmos/billblaster.js
@@ -12,40 +12,49 @@ class BillBlaster extends Gizmo {
 
   draw() {
 
-    for (let y = 0; y < this.data.dimensions.height; y++) 
+    for (let y = 0; y < this.data.dimensions.height - 1; y++)
 	{
-		if (y < this.data.dimensions.height - 2) 
+		if (y < this.data.dimensions.height - 2)
 		{
-			if ((this.data.flags & 0x4) == 0x4) 
+			if ((this.data.flags & 0x4) == 0x4)
 			{
 			    this.spriteOffset = this.scene.spriteSheetData.gizmos.bill_blaster.bulls_eye.body;
 			} else {
 			    this.spriteOffset = this.scene.spriteSheetData.gizmos.bill_blaster.default.body;
 			}
-		} 
-		else if (y == this.data.dimensions.height - 2) 
+		}
+		else
 		{
 			if ((this.data.flags & 0x4) == 0x4) {
 			    this.spriteOffset = this.scene.spriteSheetData.gizmos.bill_blaster.bulls_eye.head;
 			} else {
 			    this.spriteOffset = this.scene.spriteSheetData.gizmos.bill_blaster.default.head;
 			}
-		} 
-		else 
-		{
-			this.spriteOffset = this.scene.spriteSheetData.other.blank_tile;
 		}
-		this.canvasContext.drawImage(
-			this.scene.spriteSheet,
-			this.spriteOffset.x,
-			this.spriteOffset.y,
-			this.spriteOffset.width,
-			this.spriteOffset.height,
-			this.data.position.x,
-			(this.scene.canvas.height - this.data.position.y) - y - 1,
-			this.data.dimensions.width,
-			this.data.dimensions.height
-		);
+
+    if (y == this.data.dimensions.height - 2) {
+      this.canvasContext.drawImage(
+  			this.scene.spriteSheet,
+  			this.spriteOffset.x,
+  			this.spriteOffset.y,
+  			this.spriteOffset.width,
+  			this.spriteOffset.height,
+  			this.data.position.x,
+  			(this.scene.canvas.height - this.data.position.y) - y - 1,
+  			1, 2
+      );
+    } else {
+      this.canvasContext.drawImage(
+  			this.scene.spriteSheet,
+  			this.spriteOffset.x,
+  			this.spriteOffset.y,
+  			this.spriteOffset.width,
+  			this.spriteOffset.height,
+  			this.data.position.x,
+  			(this.scene.canvas.height - this.data.position.y) - y,
+  			1, 1
+      );
+    }
     }
   }
 }
